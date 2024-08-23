8316f4f7f43c1589e3adb35f63af4045e96e797b
kmaengggong <kmaengggong@gmail.com>
2024-08-23 13:51:36 +0900

    GEO: 패키지 정리

---
+++ src/class/4.5.ts
@@ -0,0 +1,28 @@
+interface MyStorage<T> {
+    [key: string]: T;
+}
+
+class LocalStorage<T> {
+    private storage: MyStorage<T> = {};
+
+    get(key: string): T {
+        return this.storage[key];
+    }
+    set(key: string, value: T) {
+        this.storage[key] = value;
+    }
+    remove(key: string) {
+        delete this.storage[key];
+    }
+    clear() {
+        this.storage = {};
+    }
+}
+
+const stringStorage = new LocalStorage<string>();
+stringStorage.get("btc");
+stringStorage.set("hello", "world");
+
+const booleanStorage = new LocalStorage<boolean>();
+booleanStorage.get("eth");
+booleanStorage.set("bye", true);
